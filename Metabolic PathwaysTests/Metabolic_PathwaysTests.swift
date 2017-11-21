d5630a0fe9a2d3355b13c250c2817152062130a9
Richard Liu <richy.liu.2002@gmail.com>
2017-11-20 18:59:52 -0800

    initial commit

---
+++ Metabolic PathwaysTests/Metabolic_PathwaysTests.swift
@@ -0,0 +1,36 @@
+//
+//  Metabolic_PathwaysTests.swift
+//  Metabolic PathwaysTests
+//
+//  Created by Xiaolan Zhou on 11/20/17.
+//  Copyright © 2017 Richard Liu. All rights reserved.
+//
+
+import XCTest
+@testable import Metabolic_Pathways
+
+class Metabolic_PathwaysTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
